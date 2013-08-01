fc8d2d2d6dbe5c5db9d5659e64327ea0c90c6a71
Rashi and Matt <student@appacademy.io>
2013-08-01 09:02:30 -0700

    Initial commit.

---
+++ poll/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poll::Application.config.secret_token = '18584968d19d22def8f4d5c2c21884a96f411398b69b62d8a5860fce394b65ac1a579b73714ad9db7f480a5d8e96cb9d53d0f3a8573e50a78f25670788185092'
